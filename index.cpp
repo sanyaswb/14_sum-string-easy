f4f1dffa3ba5008e33ecdaebd66c6d4e12783030
John Doe <SlySpellar@gmail.com>
2022-10-14 17:32:24 +0300

    removed solution

--- index.cpp
+++ index.cpp
@@ -3,13 +3,5 @@
 #include <cmath>
 
 int sumString(const std::string& str) {
-	int result = 0;
-
-	for (char ch : str) {
-		int converted = ch - '0';
-
-		result += converted;
-	}
-
-	return result;
+	return 0;
 }
